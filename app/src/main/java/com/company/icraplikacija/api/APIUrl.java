f5fd5fb0e7a7ef3eefd5e4a2accf021503677cf1
Stefan <stefd996@gmail.com>
2017-05-18 13:21:04 +0200

    modified IP address

--- app/src/main/java/com/company/icraplikacija/api/APIUrl.java
+++ app/src/main/java/com/company/icraplikacija/api/APIUrl.java
@@ -1,5 +1,5 @@
 package com.company.icraplikacija.api;
 
 public class APIUrl {
-    public static final String BASE_URL = "http://192.168.1.9/RetrofitICR/public/";
+    public static final String BASE_URL = "http://192.168.1.6/RetrofitICR/public/";
 }
